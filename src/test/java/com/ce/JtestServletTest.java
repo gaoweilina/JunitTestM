c85277960e89dfdbacad9d3d2db16e0ccda37271
gw <503905413@qq.com>
2019-03-21 21:34:51 +0800

    123

--- src/test/java/com/ce/JtestServletTest.java
+++ src/test/java/com/ce/JtestServletTest.java
@@ -10,5 +10,26 @@ public class JtestServletTest {
 	public void testAdd() {
 		assertEquals(2, new JtestServlet().add(1, 1));
 	}
+	@Test
+	public void testAdd2() {
+		assertEquals(3, new JtestServlet().add(1, 1));
+	}
+	@Test
+	public void testAdd3() {
+		assertEquals(2, new JtestServlet().add(1, 1));
+	}
+	@Test
+	public void testAdd4() {
+		assertEquals(3, new JtestServlet().add(1, 1));
+	}
+	@Test
+	public void testAdd5() {
+		assertEquals(3, new JtestServlet().add(1, 1));
+	}
+	@Test
+	public void testAdd6() {
+		assertEquals(2, new JtestServlet().add(1, 1));
+	}
+	
 
 }
