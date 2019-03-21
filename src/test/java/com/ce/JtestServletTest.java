ffafbd9916b06b4b07ab11f649533b54ccd8f73c
gw <503905413@qq.com>
2019-03-21 22:17:37 +0800

    321

--- src/test/java/com/ce/JtestServletTest.java
+++ src/test/java/com/ce/JtestServletTest.java
@@ -12,7 +12,7 @@ public class JtestServletTest {
 	}
 	@Test
 	public void testAdd2() {
-		assertEquals(3, new JtestServlet().add(1, 1));
+		assertEquals(2, new JtestServlet().add(1, 1));
 	}
 	@Test
 	public void testAdd3() {
@@ -20,11 +20,11 @@ public class JtestServletTest {
 	}
 	@Test
 	public void testAdd4() {
-		assertEquals(3, new JtestServlet().add(1, 1));
+		assertEquals(2, new JtestServlet().add(1, 1));
 	}
 	@Test
 	public void testAdd5() {
-		assertEquals(3, new JtestServlet().add(1, 1));
+		assertEquals(2, new JtestServlet().add(1, 1));
 	}
 	@Test
 	public void testAdd6() {
@@ -33,17 +33,17 @@ public class JtestServletTest {
 	
 	@Test
 	public void testAdd7() {
-		assertEquals(3, new JtestServlet().add(1, 1));
+		assertEquals(2, new JtestServlet().add(1, 1));
 	}
 	
 	@Test
 	public void testAdd8() {
-		assertEquals(3, new JtestServlet().add(1, 1));
+		assertEquals(2, new JtestServlet().add(1, 1));
 	}
 	
 	@Test
 	public void testAdd9() {
-		assertEquals(3, new JtestServlet().add(1, 1));
+		assertEquals(2, new JtestServlet().add(1, 1));
 	}
 	
 	@Test
